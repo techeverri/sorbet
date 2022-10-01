23e0daf31b95e42c880029d8cf2206e26ea558e4
Jake Zimmerman <zimmerman.jake@gmail.com>
2022-09-30 17:18:05 -0700

    Reset some resultType's
    Manually setting `resultType` for the `ClassOrModule` symbols to
    `nullptr` and to a `LambdaParam(tp, todo, todo)` for the `AttachedClass`
    type template mimics what would be the state had we just called
    `insertClass` for the first time, and removes all references of the
    newly-deleted type members from the containing class.
    
    This forces resolver to recompute these types, taking into account any
    deleted and added type members.

--- namer/namer.cc
+++ namer/namer.cc
@@ -1162,6 +1162,17 @@ private:
             singletonClass.data(ctx)->addLoc(ctx, ctx.locAt(klass.declLoc));
         }
 
+        // Reset resultType to nullptr for idempotency on the fast path--it will always be
+        // re-entered in resolver.
+        symbol.data(ctx)->resultType = nullptr;
+        singletonClass.data(ctx)->resultType = nullptr;
+        // TODO(jez) This is a gross hack. We are basically re-implementing the logic in
+        // singletonClass to reset the <AttachedClass> type template to what it used to be.
+        // Is there a better way to accomplish this?
+        auto todo = core::make_type<core::ClassType>(core::Symbols::todo());
+        auto tp = singletonClass.data(ctx)->members()[core::Names::Constants::AttachedClass()].asTypeMemberRef();
+        tp.data(ctx)->resultType = core::make_type<core::LambdaParam>(tp, todo, todo);
+
         // make sure we've added a static init symbol so we have it ready for the flatten pass later
         if (symbol == core::Symbols::root()) {
             ctx.state.staticInitForFile(ctx.locAt(klass.loc));
